9412c7684493c38eba507d448a1bf98b25892ea0
omid1454omid <140713936+omid1454omid@users.noreply.github.com>
2025-07-20 09:44:05 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 
 echo "✅ در حال نصب پنل شخصی‌سازی‌شده Marzban..."
 
+# نصب Docker
+curl -fsSL https://get.docker.com | bash
+apt install docker-compose -y
+
+# کلون سورس
 git clone https://github.com/omid1454omid/newmarrrzban.git /opt/marzban-custom
 cd /opt/marzban-custom
 
+# اجرای Docker Compose
 docker compose up -d
 
 echo "✅ نصب کامل شد. پنل در حال اجراست روی پورت 8080"
