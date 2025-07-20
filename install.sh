6596f2045a129b9643d5e2d84e1324306bc34d38
omid1454omid <140713936+omid1454omid@users.noreply.github.com>
2025-07-20 09:36:50 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "✅ در حال نصب پنل شخصی‌سازی‌شده Marzban..."
+
+git clone https://github.com/omid1454omid/newmarrrzban.git/opt/marzban-custom
+cd /opt/marzban-custom
+
+docker compose up -d
+
+echo "✅ نصب کامل شد. پنل در حال اجراست روی پورت 8080"
